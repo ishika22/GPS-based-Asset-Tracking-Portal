a0f5ff5fd749b26c1967af9c5f08ed190be78c4b
tanmha <tanmaymhatre40@gmail.com>
2021-03-01 23:36:12 +0530

    initialized backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# JUMBOGPS-T6
-Team ID: JUMBOGPS-T6 | Team Members: Ishika &amp; Tanmay Mhatre
---
+++ backend/src/main/java/com/crio/jumbogps/JumbogpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crio.jumbogps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumbogpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumbogpsBackendApplication.class, args);
+	}
+
+}
