68698c546d76a90944c43e670fe3d758b3f80010
Ishika Agarwal <ishuagarwal2210@gmail.com>
2021-03-14 14:40:09 +0530

    Endpoints changes

--- backend/src/main/java/com/crio/jumbogps/AssetController.java
+++ backend/src/main/java/com/crio/jumbogps/AssetController.java
@@ -3,18 +3,22 @@ package com.crio.jumbogps;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.crio.model.AssetDetail;
 import com.crio.model.AssetHistory;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @RestController
+@EnableSwagger2
 public class AssetController {
 	
 	@Autowired  
@@ -23,13 +27,13 @@ public class AssetController {
 	@Autowired  
 	private AssetDetailRepository assetDetailRepository;
 	
-	@GetMapping("/location/all")
+	@GetMapping("/location/list")
 	public List<AssetHistory> getAllAssets() {
 		getCurrentLocationOfAsset();
 		return assetHistoryRepository.findDistinctByFkAssetIdIn();
 	}
 	
-	@PostMapping("/location/current")
+	@PostMapping("/location/currentList")
 	private void getCurrentLocationOfAsset() {
 		List<AssetDetail> assetDetailList = assetDetailRepository.findAll();
 		for(AssetDetail assetDetail : assetDetailList) {
@@ -45,8 +49,19 @@ public class AssetController {
 		}
 	}
 	
-	@GetMapping("/location/{id}")
-	public List<AssetHistory> getAAssetsHistoryById(@PathVariable("id") int assetId) {
+	@GetMapping("/location/type")
+	public List<AssetHistory> getAAssetsHistoryByType(@RequestParam("type") int assetType) {
+		return assetHistoryRepository.getAssetDetailByType(assetType);
+	}
+	
+	@GetMapping("/location/activeTodayList")
+	public List<AssetHistory> getAAssetsHistoryActiveToday() {
+		return assetHistoryRepository.getAssetDetailSinceLastDay();	
+	}
+	
+	
+	@GetMapping("/location/id")
+	public List<AssetHistory> getAAssetsHistoryById(@RequestParam("id") int assetId) {
 		Optional<AssetDetail> assetDetailOptional = assetDetailRepository.findById(assetId);
 		if(assetDetailOptional.isPresent()) {
 			return assetHistoryRepository.getAssetDetailByIdSinceLastDay(assetDetailOptional.get().getPkAssetId());
@@ -55,30 +70,17 @@ public class AssetController {
 		}	
 	}
 	
-	@GetMapping("/location/type/{type}")
-	public List<AssetHistory> getAAssetsHistoryByType(@PathVariable("type") int assetType) {
-		return assetHistoryRepository.getAssetDetailByType(assetType);
-	}
-	
-	@GetMapping("/location/activeToday")
-	public List<AssetHistory> getAAssetsHistoryActiveToday() {
-		return assetHistoryRepository.getAssetDetailSinceLastDay();	
-	}
-	
-	@GetMapping("/asset/{id}")
-	public AssetHistory getAssetDetailById(@PathVariable("id") int assetId) {
-		Optional<AssetDetail> assetDetailOptional = assetDetailRepository.findById(assetId);
-		if(assetDetailOptional.isPresent()) {
-			List<AssetHistory> assetHistoryList = assetHistoryRepository.getAssetDetailById(assetId);
-			if(assetHistoryList.size()>0) {
-				return assetHistoryList.get(0);
-			}else {
-				AssetHistory assetHistory = new AssetHistory();
-				assetHistory.setFkAssetId(assetDetailOptional.get());
-				return assetHistory;
-			}
-		}else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not found");
-		}	
-	}
+	/*@GetMapping("/location/time")
+	public List<AssetHistory> getAAssetsHistoryByTime(@RequestParam("startTime") String startDate , @RequestParam("endTime") String endDate) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
+		try {
+		Date start = format.parse(startDate);
+		Date end = format.parse(endDate);
+		return assetHistoryRepository.getAssetDetailsByTime(start,end);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}*/
 }
