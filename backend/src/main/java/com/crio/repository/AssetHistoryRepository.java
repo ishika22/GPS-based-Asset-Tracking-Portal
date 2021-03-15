025b2160ff2494b48fca6682f5e87c5197e089d7
Ishika Agarwal <ishuagarwal2210@gmail.com>
2021-03-15 12:38:28 +0530

    Modularizing

---
+++ backend/src/main/java/com/crio/repository/AssetHistoryRepository.java
@@ -0,0 +1,29 @@
+package com.crio.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.crio.model.AssetHistory;
+//Performs all CRUD operations
+public interface AssetHistoryRepository extends JpaRepository<AssetHistory, Integer> {
+	
+	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId)")
+	List<AssetHistory> findDistinctByFkAssetIdIn();
+	
+	@Query("SELECT assets FROM AssetHistory assets WHERE  TIMEDIFF(NOW(), timeOfTracking) < '24:00:00' GROUP BY fkAssetId")
+	List<AssetHistory> getAssetDetailSinceLastDay();
+	
+	@Query("SELECT assets FROM AssetHistory assets WHERE  fkAssetId.pkAssetId = :assetId AND TIMEDIFF(NOW(), timeOfTracking) < '24:00:00'")
+	List<AssetHistory> getAssetDetailByIdSinceLastDay(@Param("assetId") Integer assetId);
+	
+	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId) AND fkAssetId.fkAssetType.pkAssetTypeId = :assetType")
+	List<AssetHistory> getAssetDetailByType(@Param("assetType") Integer assetType);
+	
+	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking >= :startDate and timeOfTracking <= :endDate")
+	List<AssetHistory> getAssetDetailsByTime(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
+	
+}
