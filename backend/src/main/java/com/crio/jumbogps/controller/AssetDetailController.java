e3148e3a1bc630bab3034224f35a7bd7af40694d
tanmha <tanmaymhatre40@gmail.com>
2021-03-29 21:18:17 +0530

    added CORS support

--- backend/src/main/java/com/crio/jumbogps/controller/AssetDetailController.java
+++ backend/src/main/java/com/crio/jumbogps/controller/AssetDetailController.java
@@ -3,6 +3,7 @@ package com.crio.jumbogps.controller;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.crio.jumbogps.model.AssetDetail;
 import com.crio.jumbogps.repository.AssetDetailRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class AssetDetailController {
 	
 	@Autowired  
--- backend/src/main/java/com/crio/jumbogps/controller/UserController.java
+++ backend/src/main/java/com/crio/jumbogps/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import com.crio.jumbogps.security.JwtUtil;
 import com.crio.jumbogps.service.JwtUserDetailService;;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	@Autowired  
