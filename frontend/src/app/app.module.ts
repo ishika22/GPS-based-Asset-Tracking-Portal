1faac36ab8245632d1702338146cd5ae59e842c2
tanmha <tanmaymhatre40@gmail.com>
2021-04-11 00:52:24 +0530

    firebase config

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -68,8 +68,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       MatFormFieldModule,
       ServiceWorkerModule.register('ngsw-worker.js', {
         enabled: environment.production,
-        // Register the ServiceWorker as soon as the app is stable
-        // or after 30 seconds (whichever comes first).
         registrationStrategy: 'registerWhenStable:30000'
       }),
   ],
