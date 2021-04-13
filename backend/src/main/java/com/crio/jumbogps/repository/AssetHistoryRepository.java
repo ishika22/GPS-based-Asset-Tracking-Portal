b2ae08de64348aaa7249b7bac868e0d6989de352
Ishika <ishuagarwal2210@gmail.com>
2021-04-14 01:03:57 +0530

    enable multi filter

--- backend/src/main/java/com/crio/jumbogps/repository/AssetHistoryRepository.java
+++ backend/src/main/java/com/crio/jumbogps/repository/AssetHistoryRepository.java
@@ -20,14 +20,14 @@ public interface AssetHistoryRepository extends JpaRepository<AssetHistory, Inte
 	@Query(value ="SELECT assets FROM AssetHistory assets WHERE  fkAssetId.pkAssetId = :assetId AND TIMEDIFF(NOW(), timeOfTracking) < '24:00:00'")
 	List<AssetHistory> getAssetDetailByIdSinceLastDay(@Param("assetId") Integer assetId);
 	
-	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId) AND fkAssetId.fkAssetType.pkAssetTypeId = :assetType")
+	@Query("SELECT assets FROM AssetHistory assets WHERE fkAssetId.fkAssetType.pkAssetTypeId = :assetType AND timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId)")
 	List<AssetHistory> getAssetDetailByType(@Param("assetType") Integer assetType);
 	
 	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking >= :startDate and timeOfTracking <= :endDate AND timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId)")
 	List<AssetHistory> getAssetDetailsByTime(@Param("startDate") LocalDateTime start, @Param("endDate") LocalDateTime end);
 	
-	@Query("SELECT assets FROM AssetHistory assets WHERE timeOfTracking >= :startTime and timeOfTracking <= :endTime AND fkAssetId.pkAssetId = :assetId")
-	List<AssetHistory> getAssetDetailsByIdAndTime(int assetId, LocalDateTime startTime, LocalDateTime endTime);
+	@Query("SELECT assets FROM AssetHistory assets WHERE fkAssetId.fkAssetType.pkAssetTypeId = :assetType AND timeOfTracking >= :startTime and timeOfTracking <= :endTime AND timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory GROUP BY fkAssetId)")
+	List<AssetHistory> getAssetDetailsByTypeAndTime(@Param("assetType") Integer assetType,@Param("startTime") LocalDateTime startTime,@Param("endTime") LocalDateTime endTime);
 	
 	@Query("SELECT assets FROM AssetHistory assets WHERE fkAssetId.pkAssetId = :assetId and timeOfTracking IN(SELECT MAX(timeOfTracking) FROM AssetHistory where fkAssetId.pkAssetId = :assetId) ")
 	AssetHistory getCurrentLocationOfAsset(@Param("assetId") Integer pkAssetId);
