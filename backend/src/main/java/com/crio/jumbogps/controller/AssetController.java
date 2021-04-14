6159d57d553ec067ed537d80f445ae99e4b13f20
ishika22 <ishuagarwal2210@gmail.com>
2021-04-14 20:12:12 +0530

    Update AssetController.java

--- backend/src/main/java/com/crio/jumbogps/controller/AssetController.java
+++ backend/src/main/java/com/crio/jumbogps/controller/AssetController.java
@@ -120,12 +120,14 @@ public class AssetController {
 	
 	@GetMapping("/location/time/type")
 	public List<AssetHistory> getAssetsHistoryByIdAndTime(
-		 @RequestParam("type") Integer assetType,
+		 @RequestParam("type") String assetType,
 		 @RequestParam("startTime")@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime ,
 		 @RequestParam("endTime")  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime) {
 		
 		try {
-			
+			if(assetType == null || assetType.contentEquals("undefined")){
+				return getAssetsHistoryByTime(startTime,endTime);
+			}
 			return assetHistoryRepository.getAssetDetailsByTypeAndTime(assetType,startTime,endTime);
 		}catch(Exception e) {
 			e.printStackTrace();
